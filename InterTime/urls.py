c45df2e73170515bd5e8be19c313a4db5ce2a477
jnphilipp <jnphilipp@gmail.com>
2013-11-15 13:43:53 +0100

    Basic views and templates for timetable.

--- InterTime/urls.py
+++ InterTime/urls.py
@@ -1,12 +1,10 @@
 from django.conf.urls import patterns, include, url
-
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'InterTime.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
+	url(r'^$', 'timetable.views.index'),
+	url(r'^timetable/$', 'timetable.views.index'),
+	url(r'^timetable/(?P<modul_id>\d+)/$', 'timetable.views.details', name='blog-post'),
+	url(r'^admin/', include(admin.site.urls)),
 )
--- timetable/views.py
+++ timetable/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, get_list_or_404, render
+from timetable.models import Modul
 
-# Create your views here.
+def index(request):
+	moduls = get_list_or_404(Modul.objects.order_by('number'))
+
+	return render(request, 'timetable/index.html', {'moduls':moduls})
+
+def details(request, modul_id):
+	modul = get_object_or_404(Modul, pk=modul_id)
+
+	return render(request, 'timetable/details.html', {'modul': modul})
