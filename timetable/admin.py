a361ac95954041a28af3d9ec04b6eb3c1da71ef5
jnphilipp <jnphilipp@gmail.com>
2013-11-15 13:20:18 +0100

    Admin for timetable.

--- timetable/admin.py
+++ timetable/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from django.forms import TextInput
+from django.db import models
+from timetable.models import Modul
 
-# Register your models here.
+class ModulAdmin(admin.ModelAdmin):
+	list_display = ('name',)
+
+	formfield_overrides = {
+		models.CharField: {'widget': TextInput(attrs={'autocomplete':'off'})},
+	}
+
+	fieldsets = [
+		(None, {'fields': ['number', 'name']}),
+	]
+
+admin.site.register(Modul, ModulAdmin)
