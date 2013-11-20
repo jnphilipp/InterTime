9d7712d61187ff26039e3b0291f1f63fccce2797
jnphilipp <jnphilipp@gmail.com>
2013-11-20 17:11:39 +0100

    Updated admin.

--- timetable/admin.py
+++ timetable/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
 from django.forms import TextInput
 from django.db import models
-from timetable.models import Deparment, Event, EventType, Location, Instructor, Modul, ModulType, Semester, Source
+from timetable.models import Deparment, Event, EventType, FieldOfStudy, Location, Instructor, Modul, ModulType, Selection, Semester, Source, Student
 
 class SourceAdmin(admin.ModelAdmin):
 	list_display = ('url', 'regex')
@@ -118,12 +120,38 @@ class EventAdmin(admin.ModelAdmin):
 
 	filter_horizontal = ('instructors',)
 
+class StudentAdmin(admin.ModelAdmin):
+	list_display = ('user',)
+
+	formfield_overrides = {
+		models.CharField: {'widget': TextInput(attrs={'autocomplete':'off'})},
+	}
+
+	filter_horizontal = ('field_of_studies',)
+
+class SelectionAdmin(admin.ModelAdmin):
+	filter_horizontal = ('events',)
+
+class StudentInline(admin.StackedInline):
+	model = Student
+	can_delete = False
+	filter_horizontal = ('field_of_studies',)
+
+class UserAdmin(UserAdmin):
+	inlines = (StudentInline, )
+
 admin.site.register(Deparment, DeparmentAdmin)
 admin.site.register(Event, EventAdmin)
 admin.site.register(EventType)
+admin.site.register(FieldOfStudy)
 admin.site.register(Location, LocationAdmin)
 admin.site.register(Instructor, InstructorAdmin)
 admin.site.register(Modul, ModulAdmin)
 admin.site.register(ModulType, ModulTypeAdmin)
-admin.site.register(Source, SourceAdmin)
+admin.site.register(Selection, SelectionAdmin)
 admin.site.register(Semester, SemesterAdmin)
+admin.site.register(Source, SourceAdmin)
+admin.site.register(Student, StudentAdmin)
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
