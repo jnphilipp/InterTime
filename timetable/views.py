746823ea1e4f5def7b15746d40e97fca6cc10e81
mam10dfd <mam10dfd@studserv.uni-leipzig.de>
2013-11-21 11:15:40 +0100

    nicht ganz funktionierende Änderung

--- timetable/views.py
+++ timetable/views.py
@@ -4,21 +4,26 @@ from timetable.models import Deparment
 
 
 def index(request):
-	return render(request, 'index.html')
+    return render(request, 'index.html')
+
 
 def moduls(request):
-	moduls = get_list_or_404(Modul.objects.order_by('number'))
-	return render(request, 'timetable/index.html', {'moduls':moduls})
+    moduls = list(Modul.objects.order_by('number'))
+    return render(request, 'timetable/index.html', {'moduls': moduls})
+
 
 def details(request, modul_id):
-	modul = get_object_or_404(Modul, pk=modul_id)
-	return render(request, 'timetable/details.html', {'modul': modul})
+    modul = get_object_or_404(Modul, pk=modul_id)
+    return render(request, 'timetable/details.html', {'modul': modul})
 
 
 def departments(request):
-	departments = get_list_or_404(Deparment.objects.order_by('name'))
-	return render(request, 'timetable/departments.html', {'departments':departments})
+    departments = get_list_or_404(Deparment.objects.order_by('name'))
+    return render(request, 'timetable/departments.html', {'departments': departments})
+
 
 def department_details(request, department_id):
-	department = get_object_or_404(Deparment, pk=department_id)
-	return render(request, 'timetable/department_details.html', {'department': department})
+    department = get_object_or_404(Deparment, pk=department_id)
+    return render(request, 'timetable/department_details.html', {'department': department})
+
+
