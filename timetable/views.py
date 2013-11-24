ca5035f3d80660a9f8fff6b971f194bf91df0a56
jnphilipp <jnphilipp@gmail.com>
2013-11-24 23:11:06 +0100

    Some style changes.

--- timetable/views.py
+++ timetable/views.py
@@ -1,29 +1,26 @@
 from django.shortcuts import get_object_or_404, get_list_or_404, render
-from timetable.models import Modul
-from timetable.models import Deparment
-from timetable.models import Event
+from timetable.models import Deparment, Event, Modul
 
 def index(request):
-    return render(request, 'index.html')
-
+	return render(request, 'index.html')
 
 def moduls(request):
 	moduls = Modul.objects.all().order_by('number')
 	return render(request, 'timetable/index.html', {'moduls':moduls})
 
 def details(request, modul_id):
-    modul = get_object_or_404(Modul, pk=modul_id)
-    return render(request, 'timetable/details.html', {'modul': modul})
+	modul = get_object_or_404(Modul, pk=modul_id)
+	return render(request, 'timetable/details.html', {'modul': modul})
 
 def departments(request):
 	departments = Deparment.objects.all().order_by('name')
 	return render(request, 'timetable/departments.html', {'departments':departments})
 
 def department_details(request, department_id):
-    department = get_object_or_404(Deparment, pk=department_id)
-    return render(request, 'timetable/department_details.html', {'department': department})
+	department = get_object_or_404(Deparment, pk=department_id)
+	return render(request, 'timetable/department_details.html', {'department': department})
 
 def plan(request):
-    ws_moduls = Modul.objects.all().order_by('number').filter(event__semester__exact='1').values('number','name','lp','modultype','description').distinct()
-    ss_moduls = Modul.objects.all().order_by('number').filter(event__semester__exact='0')
-    return render(request, 'timetable/plan.html',{'ws_moduls': ws_moduls,'ss_moduls': ss_moduls})
+	ws_moduls = Modul.objects.all().order_by('number').filter(event__semester__exact='1').values('number','name','lp','modultype','description').distinct()
+	ss_moduls = Modul.objects.all().order_by('number').filter(event__semester__exact='0')
+	return render(request, 'timetable/plan.html',{'ws_moduls': ws_moduls,'ss_moduls': ss_moduls})
